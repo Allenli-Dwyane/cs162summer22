c2a6b5d71cf8554c727f9d67a52b776c0403e2da
junzheli <allenli-@pku.edu.cn>
2022-08-04 15:56:19 +0000

    finish limits.c

--- hw-intro/limits.c
+++ hw-intro/limits.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
 #include <sys/resource.h>
+#include <unistd.h>
+#include <stdlib.h>
+#define CUR_PROC 0
 
 int main() {
     struct rlimit lim;
-    printf("stack size: %ld\n", 0L);
-    printf("process limit: %ld\n", 0L);
-    printf("max file descriptors: %ld\n", 0L);
+    /* get stack size */
+    if(prlimit(CUR_PROC, RLIMIT_STACK, NULL, &lim) == -1)
+        perror("prlimit-1");
+    printf("stack size: %ld\n", lim.rlim_cur);
+    if(prlimit(CUR_PROC, RLIMIT_NPROC, NULL, &lim) == -1)
+        perror("prlimit-1");
+    printf("process limit: %ld\n", lim.rlim_cur);
+    if(prlimit(CUR_PROC, RLIMIT_NOFILE, NULL, &lim) == -1)
+        perror("prlimit-1");
+    printf("max file descriptors: %ld\n", lim.rlim_cur);
     return 0;
 }
